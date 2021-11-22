d310ee240458f192f5d663dd57bbcd0d5a14c02d
tomstark99 <jb18789@bristol.ac.uk>
2021-11-22 12:45:43 +0000

    added placeholder Trainer class

---
+++ coursework/Trainer.py
@@ -0,0 +1,184 @@
+import time
+from multiprocessing import cpu_count
+from typing import Union, NamedTuple
+
+import torch
+import torch.backends.cudnn
+import numpy as np
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        criterion: nn.Module,
+        optimizer: Optimizer,
+        summary_writer: SummaryWriter,
+        device: torch.device,
+    ):
+        self.model = model.to(device)
+        self.device = device
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.summary_writer = summary_writer
+        self.step = 0
+
+    def train(
+        self,
+        epochs: int,
+        val_frequency: int,
+        print_frequency: int = 20,
+        log_frequency: int = 5,
+        start_epoch: int = 0
+    ):
+        self.model.train()
+        for epoch in tqdm(
+            range(start_epoch, epochs),
+            unit=" epoch",
+            dynamic_ncols=True
+        ):
+            self.model.train()
+            data_load_start_time = time.time()
+            for batch, labels in self.train_loader:
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                data_load_end_time = time.time()
+
+                self.optimizer.zero_grad()
+
+                logits = self.model.forward(batch)
+
+                loss = self.criterion(logits, labels)
+                loss.backward()
+
+                self.optimizer.step()
+
+                with torch.no_grad():
+                    preds = logits.argmax(-1)
+                    accuracy = compute_accuracy(labels, preds)
+
+                data_load_time = data_load_end_time - data_load_start_time
+                step_time = time.time() - data_load_end_time
+                if ((self.step + 1) % log_frequency) == 0:
+                    self.log_metrics(epoch, accuracy, loss, data_load_time, step_time)
+                if ((self.step + 1) % print_frequency) == 0:
+                    self.print_metrics(epoch, accuracy, loss, data_load_time, step_time)
+
+                self.step += 1
+                data_load_start_time = time.time()
+
+            self.summary_writer.add_scalar("epoch", epoch, self.step)
+            if ((epoch + 1) % val_frequency) == 0:
+                self.validate()
+                # self.validate() will put the model in validation mode,
+                # so we have to switch back to train mode afterwards
+                self.model.train()
+
+    
+    def compute_accuracy(
+        labels: Union[torch.Tensor, np.ndarray], 
+        preds: Union[torch.Tensor, np.ndarray]
+    ) -> float:
+        """
+        Args:
+            labels: ``(batch_size, class_count)`` tensor or array containing example labels
+        """
+        return float((labels == preds).sum()) / len(labels)
+
+    def print_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        epoch_step = self.step % len(self.train_loader)
+        print(
+                f"epoch: [{epoch}], "
+                f"step: [{epoch_step}/{len(self.train_loader)}], "
+                f"batch loss: {loss:.5f}, "
+                f"batch accuracy: {accuracy * 100:2.2f}, "
+                f"data load time: "
+                f"{data_load_time:.5f}, "
+                f"step time: {step_time:.5f}"
+        )
+
+    def print_per_class_accuracy(self):
+        classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        correct_pred = {classname: 0 for classname in classes}
+        total_pred = {classname: 0 for classname in classes}
+
+        self.model.eval()
+        with torch.no_grad():
+            for batch, labels in self.val_loader:
+                logits = self.model(batch.to(self.device))
+
+                preds = logits.argmax(dim=-1).cpu().numpy()
+
+                for label, pred in zip(labels.cpu().numpy(), preds):
+                    if label == pred:
+                        correct_pred[classes[label]] += 1
+                    total_pred[classes[label]] += 1
+        
+        for classname, correct in correct_pred.items():
+            accuracy = 100 * float(correct) / total_pred[classname]
+            print("Accuracy for class {:5s} is: {:.1f}%".format(classname, accuracy))
+
+    def log_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        self.summary_writer.add_scalar("epoch", epoch, self.step)
+        self.summary_writer.add_scalars(
+                "accuracy",
+                {"train": accuracy},
+                self.step
+        )
+        self.summary_writer.add_scalars(
+                "loss",
+                {"train": float(loss.item())},
+                self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", data_load_time, self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", step_time, self.step
+        )
+
+    def validate(self):
+        results = {"preds": [], "labels": []}
+        total_loss = 0
+        self.model.eval()
+
+        # No need to track gradients for validation, we're not optimizing.
+        with torch.no_grad():
+            for batch, labels in self.val_loader:
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                logits = self.model(batch)
+                loss = self.criterion(logits, labels)
+                total_loss += loss.item()
+                preds = logits.argmax(dim=-1).cpu().numpy()
+                results["preds"].extend(list(preds))
+                results["labels"].extend(list(labels.cpu().numpy()))
+
+        accuracy = compute_accuracy(
+            np.array(results["labels"]), np.array(results["preds"])
+        )
+        average_loss = total_loss / len(self.val_loader)
+
+        self.summary_writer.add_scalars(
+                "accuracy",
+                {"test": accuracy},
+                self.step
+        )
+        self.summary_writer.add_scalars(
+                "loss",
+                {"test": average_loss},
+                self.step
+        )
+        print(f"validation loss: {average_loss:.5f}, accuracy: {accuracy * 100:2.2f}")
+        self.print_per_class_accuracy()
