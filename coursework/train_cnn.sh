21a16ab3bbac760f947fafee11a6748436c68fa9
omarinio <jb18940@bristol.ac.uk>
2021-12-02 19:17:50 +0000

    runs

--- coursework/train_cnn.sh
+++ coursework/train_cnn.sh
@@ -9,5 +9,5 @@ module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
 
-python train_cnn.py --epoch 150 --batch-size 64 --learning-rate 1e-3 --use-cuda --full-train
+python train_cnn.py --epoch 200 --batch-size 32 --learning-rate 1e-3 --use-cuda --full-train --data-aug-hflip
 
--- coursework/train_cnn_full_hflip.sh
+++ coursework/train_cnn_full_hflip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#SBATCH --account comsm0045
+#SBATCH --reservation=comsm0045-coursework
 #SBATCH --partition gpu
 #SBATCH --time 0-01:00
 #SBATCH --mem 64GB
@@ -9,4 +11,4 @@ module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
 
-python train_cnn.py --batch-size 64 --learning-rate 1e-3 --use-cuda --full-train --epochs 150 --val-frequency 1 --data-aug-hflip
+python train_cnn.py --batch-size 32 --learning-rate 1e-3 --use-cuda --full-train --epochs 200 --val-frequency 1 --data-aug-hflip
