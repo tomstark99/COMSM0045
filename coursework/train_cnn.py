616d68113fdc6e48fcbe9fe2d6dc7f0fa3d5086a
Omar Suleiman <jb18940@bristol.ac.uk>
2021-12-04 23:08:34 +0000

    added args for different augmentations

--- coursework/train_cnn.py
+++ coursework/train_cnn.py
@@ -16,8 +16,8 @@ from torch.utils.tensorboard import SummaryWriter
 from dataset import DCASE, NF_DCASE, V_DCASE
 from trainer import Trainer
 from CNN import CNN
-# from torchvision import transforms
-from transforms import HorizontalFlip
+from torchvision.transforms import Compose
+from transforms import HorizontalFlip, RandomNoise, RandomSplit
 
 parser = argparse.ArgumentParser(
     description="CW CNN training for DCASE 2016",
@@ -103,6 +103,18 @@ parser.add_argument(
     action="store_true"
 )
 
+parser.add_argument(
+    "--data-noise", 
+    default=False,
+    action="store_true"
+)
+
+parser.add_argument(
+    "--data-split", 
+    default=False,
+    action="store_true"
+)
+
 def train_test_loader(dataset: DCASE, batch_size: int, val_split: float) -> Tuple[DataLoader, DataLoader]:
     
     labels = {label: [clips for clips in dataset._labels[dataset._labels['label']==label].clip_no.unique()] for label in dataset._labels['label']}
@@ -164,12 +176,20 @@ def main(args):
     print(f'writing logs to {log_dir}')
 
     summary_writer = SummaryWriter(str(log_dir), flush_secs=1)
-
+    
+    transform = None
+    transforms_ = []
     if args.data_aug_hflip:
-        transform = HorizontalFlip()
-        train_dataset = DCASE(root_dir_train, clip_length, transform=transform)
-    else:
-        train_dataset = DCASE(root_dir_train, clip_length)
+        transforms_.append(HorizontalFlip())
+    if args.data_noise:
+        transforms_.append(RandomNoise())
+    if args.data_split:
+        transforms_.append(RandomSplit())
+    
+    if transforms_:
+        transform = Compose(transforms_)
+
+    train_dataset = DCASE(root_dir_train, clip_length, transform=transform)
 
     model = CNN(clip_length, train_dataset.get_num_clips())
     optim = Adam(model.parameters(), lr=args.learning_rate)
@@ -228,9 +248,9 @@ def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
         untangle in TB).
     """
     if args.full_train:
-        tb_log_dir_prefix = f'full_CNN_bs={args.batch_size}_lr={args.learning_rate}_run_' + ("hflip_" if args.data_aug_hflip else "")
+        tb_log_dir_prefix = f'full_CNN_bs={args.batch_size}_lr={args.learning_rate}_run_' + ("hflip_" if args.data_aug_hflip else "") + ("noise_" if args.data_noise else "") + ("split_" if args.data_split else "")
     else:
-        tb_log_dir_prefix = f'non_full_CNN_bs={args.batch_size}_lr={args.learning_rate}_run_' + ("hflip_" if args.data_aug_hflip else "")
+        tb_log_dir_prefix = f'non_full_CNN_bs={args.batch_size}_lr={args.learning_rate}_run_' + ("hflip_" if args.data_aug_hflip else "") + ("noise_" if args.data_noise else "") + ("split_" if args.data_split else "")
 
     i = 0
     while i < 1000:
--- coursework/transforms.py
+++ coursework/transforms.py
@@ -7,17 +7,33 @@ class HorizontalFlip(object):
         self.p = p
 
     def __call__(self, sample):
-        # x = np.random.choice([0,1], p=[1-self.p, self.p])
+        x = np.random.choice([0,1], p=[1-self.p, self.p])
+        #flip
+        if x == 1:
+            sample = torch.flip(sample, dims=[2])
+
+        return sample
+
+class RandomNoise(object):
+    def __init__(self, p: float = 0.5):
+        self.p = p
+
+    def __call__(self, sample):
         y = np.random.choice([0,1], p=[1-self.p, self.p])
-        z = np.random.choice([0,1], p=[1-self.p, self.p])
-        # flip
-        # if x == 1:
-        #     sample = torch.flip(sample, dims=[2])
         # noise
         if y == 1:
             # noise = np.random.randn(len(sample))
             noise = torch.randn(sample.size())
             sample += 2 * noise
+
+        return sample
+
+class RandomSplit(object):
+    def __init__(self, p: float = 0.5):
+        self.p = p
+
+    def __call__(self, sample):
+        z = np.random.choice([0,1], p=[1-self.p, self.p])
         # split
         if z == 1:
             length = sample.shape[2]
@@ -26,4 +42,3 @@ class HorizontalFlip(object):
             sample = torch.cat([new2, new], dim=2)
 
         return sample
-
