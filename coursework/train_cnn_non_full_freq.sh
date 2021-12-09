6293f4a9bb6958b37fda78465a7b202e162ed4e9
omarinio <jb18940@bristol.ac.uk>
2021-12-09 12:31:22 +0000

    sh file

---
+++ coursework/train_cnn_non_full_freq.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#SBATCH --account comsm0045
+#SBATCH --reservation=comsm0045-coursework
+#SBATCH --partition gpu
+#SBATCH --time 0-01:30
+#SBATCH --mem 100GB
+#SBATCH --gres gpu:1
+
+# get rid of any modules already loaded
+module purge
+# load in the module dependencies for this script
+module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
+
+python train_cnn.py --batch-size 64 --learning-rate 1e-3 --use-cuda --val-frequency 1 --freq-mask --time-mask
+
+
